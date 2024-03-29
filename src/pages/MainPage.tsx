316eba007099386a1b4dae606af79b2c977ebc59
Ekaterina Bondareva <1978231@bk.ru>
2024-03-30 02:43:43 +0500

    cleaned extra code

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,17 +9,6 @@ interface Props {
 }
 
 export const MainPage: React.FC<Props> = ({ comments }) => {
-  // const [allComments, setAllComments] = useState<Comment[]>([]);
-
-  // useEffect(() => {
-  //   fetch("./data/comments.json")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setAllComments(data);
-  //     })
-  //     .catch((err) => console.log("Ошибка", err));
-  // }, []);
-
   return (
     <List data={comments} height={800} itemHeight={100} itemKey="id">
       {(comment) => <Post commentInformation={comment} />}
